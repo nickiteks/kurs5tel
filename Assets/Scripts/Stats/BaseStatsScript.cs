845394dde1ef8b7f0616d38060e92f26d6526f0a
nickiteks <55086354+nickiteks@users.noreply.github.com>
2020-11-16 20:08:09 +0400

    start

---
+++ Assets/Scripts/Stats/BaseStatsScript.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseStatsScript : MonoBehaviour
+{
+    [SerializeField]
+    private int manaMax;
+    [SerializeField]
+    private int mana;
+    [SerializeField]
+    private int health;
+    [SerializeField]
+    private int maxHelth;
+    [SerializeField]
+    private int armor;
+    [SerializeField]
+    private int damage;
+
+    public int Mana
+    {
+        get
+        {
+            return mana;
+        }
+        set
+        {
+            mana = value;
+        }
+    }
+    public int ManaMax
+    {
+        get
+        {
+            return manaMax;
+        }
+        set
+        {
+            manaMax = value;
+        }
+    }
+    public int Health
+    {
+        get
+        {
+            return health;
+        }
+        set
+        {
+            health = value;
+            if (health <= 0)
+            {
+                Death();
+            }
+            if (health > maxHelth)
+            {
+                health = maxHelth;
+            }
+        }
+    }
+    public void SetDamage(int damage)
+    {
+        Health -= (damage - armor);
+    }
+    private void Death()
+    {
+        Debug.Log("смерть");
+    }
+    public int MaxHelth
+    {
+        get
+        {
+            return maxHelth;
+        }
+        set
+        {
+            maxHelth = value;
+        }
+    }
+    public int Armor
+    {
+        get
+        {
+            return armor;
+        }
+        set
+        {
+            armor = value;
+        }
+    }
+    private void Awake()
+    {
+        health = maxHelth;
+    }
+    public int Damage
+    {
+        get
+        {
+            return damage;
+        }
+        set
+        {
+            damage = value;
+        }
+    }
+}
---
+++ Assets/Scripts/movement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private float horizontal;
+    private float vertical;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+        rb.velocity = new Vector2(horizontal, vertical);
+
+
+    }
+    //
+    void Awake()
+    {
+
+    }
+}
