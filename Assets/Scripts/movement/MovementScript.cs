776c61966b170b1512f7e5eca1d99725f93ee51b
Cmanchik <aksenova1648@gmail.com>
2020-11-21 15:04:12 +0300

    Revert "Revert "Передвижение игрока""
    This reverts commit 6fabea97d19fa1ce50dba7c8f68110c31f7aa6f6.

---
+++ Assets/Scripts/movement/MovementScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class MovementScript : MonoBehaviour
+{
+    [SerializeField]
+    private int speed = 1;
+    private Rigidbody2D rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    public void Move(float axisVertical, float axisHorizontal)
+    {
+        rb.velocity = new Vector2(axisHorizontal, axisVertical) * speed;
+
+        if (axisHorizontal < 0) transform.rotation = Quaternion.Euler(new Vector2(0, 180));
+        else transform.rotation = Quaternion.Euler(new Vector2(0, 0));
+    }
+}
