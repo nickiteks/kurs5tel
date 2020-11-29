7d5abdcca3b73cea06123c8ba11a2303d4e0fb77
Cmanchik <aksenova1648@gmail.com>
2020-11-29 18:28:13 +0300

    Движение по вертикали и горизонтали

---
+++ Assets/Scripts/Utils/InputManager.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : Singleton<InputManager>
+{
+    public KeyCode moveRight = KeyCode.D;
+    public KeyCode moveLeft = KeyCode.A;
+    public KeyCode moveUp = KeyCode.W;
+    public KeyCode moveDown = KeyCode.S;
+}
