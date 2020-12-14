f962b22391aa3ee2921f5a501611dec21358cbf9
nickiteks <55086354+nickiteks@users.noreply.github.com>
2020-12-15 00:05:40 +0400

    добавлен скейлинг брони

--- Assets/Scripts/Stats/StatsInventoryManager.cs
+++ Assets/Scripts/Stats/StatsInventoryManager.cs
@@ -11,18 +11,38 @@ public class StatsInventoryManager : MonoBehaviour
     [SerializeField]
     private EventSystem es;
     [SerializeField]
-    private StatsContainer StatsContainer;
+    private BaseStatsScript StatsContainer;
+    public Database database = new Database();
+    public MovingObgectManager movingObgect;
     public void CellScanner()
     {
         int itemIndex = int.Parse(es.currentSelectedGameObject.name);
-        ItemInventory item = inventory.items[itemIndex];
-        Debug.Log(item);
+        if (movingObgect.ItemInventory == null)
+        {
+            StatsContainer.Armor += database.items[inventory.items[itemIndex].id].armor;
+        }
+        else
+        {
+            StatsContainer.Armor -= database.items[movingObgect.ItemInventory.id].armor;
+        }
+        if (StatsContainer.Armor < 0)
+        {
+            StatsContainer.Armor = 0;
+        }
+    }
+    public void InventiryScanner()
+    {
+        StatsContainer.Armor = 0;
+        foreach (var item in inventory.items)
+        {
+            StatsContainer.Armor += database.items[item.id].armor;
+        }
     }
     private void Start()
     {
+        InventiryScanner();
         Button[] buttons = GetComponentsInChildren<Button>();
-        Debug.Log(buttons.Length);
-        foreach( Button button in buttons)
+        foreach (Button button in buttons)
         {
             button.onClick.AddListener(delegate { CellScanner(); });
         }
