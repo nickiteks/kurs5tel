7ec9ae07c0e55c1265b0cb119728bd305de3ec55
D4Gj <inzadimonax@gmail.com>
2020-12-18 22:56:19 +0400

    FightLogicStarted

---
+++ Assets/Scripts/Fight/CharacterFightController/EnemyFightController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFightController : FightController
+{
+    public List<Character> opponents { get; set; }
+
+    protected override void ApplyActionToTarget(Character target)
+    {
+        throw new System.NotImplementedException();
+    }
+}
