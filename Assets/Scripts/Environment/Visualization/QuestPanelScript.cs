167541aad257abe58fa3ee92307a9833b2cfe98c
nickiteks <55086354+nickiteks@users.noreply.github.com>
2020-12-19 00:02:11 +0400

    environment добавлен

---
+++ Assets/Scripts/Environment/Visualization/QuestPanelScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class QuestPanelScript : MonoBehaviour
+{
+    TextMeshPro mainText;
+    TextMeshPro quickText;
+    RectTransform rewardItemsPanel;
+    QuestMagazine questMagazine;
+
+    private void OnEnable()
+    {
+        
+    }
+
+    public void GetQuest()
+    {
+
+    }
+
+    public void CompleteQuest()
+    {
+
+    }
+}
