fae2dfe39fe24705d8135f927190b46c35a60e16
Cmanchik <aksenova1648@gmail.com>
2020-12-20 12:09:17 +0300

    Создал контроллеры и систему преследования

---
+++ Assets/Scripts/Following/MainPlayerFollowScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainPlayerFollowScript : MonoBehaviour
+{
+    [SerializeField]
+    private List<FollowScript> followers = new List<FollowScript>();
+
+    public void CreateFollowPoint(FollowPoint followPoint)
+    {
+        foreach (FollowScript follower in followers)
+        {
+            follower.AddFollowPoint(followPoint);
+        }
+    }
+}
