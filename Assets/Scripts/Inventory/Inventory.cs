10ec7971ce6ebadbc4c3e57fea651acec22e0f69
nickiteks <55086354+nickiteks@users.noreply.github.com>
2020-12-26 01:39:47 +0400

    add logs

--- Assets/Scripts/Inventory/Inventory.cs
+++ Assets/Scripts/Inventory/Inventory.cs
@@ -39,8 +39,10 @@ public class Inventory : MonoBehaviour
 
     ExceptionsInventory ExceptionsInventory;
     ValidationInventory validationInventory;
+    private Logger logger;
     public void Awake()
     {
+        logger = new Logger("InventoryLog");
         movingObgectManager = MovingObgectManager.Instance;
         movingObgectManager.ItemInventory = null;
         if (items.Count == 0)
@@ -50,7 +52,7 @@ public class Inventory : MonoBehaviour
         
         for (int i = 0; i < maxCount; i++)//тест заполнения
         {
-            AddItem(i, data.items[Random.Range(0,2)], Random.Range(1, cellSize));
+            AddItem(i, data.items[Random.Range(0,6)], Random.Range(1, cellSize));
         }
         es = FindObjectOfType<EventSystem>();
         UpdateInventiory();
@@ -87,6 +89,7 @@ public class Inventory : MonoBehaviour
         {
             items[id].itemGameObject.GetComponentInChildren<TMP_Text>().text = "";
         }
+        logger.Log("Добавлен предмет " + item.id);
     }
     /// <summary>
     /// добавление уже существующего ItemInventory  в инвентарь
