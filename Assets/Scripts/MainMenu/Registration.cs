b1878c49fdc28e25d27fb5ca901a18faa32c76b8
KDANext <dima.1998.gig@mail.ru>
2020-12-28 15:10:36 +0400

    StartWork

---
+++ Assets/Scripts/MainMenu/Registration.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Registration : MonoBehaviour
+{
+    private readonly string APINameFunction = "Register";
+    private readonly string APINameController = "Client";
+
+    public IEnumerator Register()
+    {
+        string url = APIConfig.url();
+        using (WWW www = new WWW(url))
+        {
+            yield return www;
+            Renderer renderer = GetComponent<Renderer>();
+            renderer.material.mainTexture = www.texture;
+        }
+    }
+}
