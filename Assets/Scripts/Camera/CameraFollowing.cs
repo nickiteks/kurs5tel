036e1c1aa1d0991627b0e270f559d21d782ededa
Cmanchik <aksenova1648@gmail.com>
2020-12-28 13:40:57 +0300

    Scroll Camera

--- Assets/Scripts/Camera/CameraFollowing.cs
+++ Assets/Scripts/Camera/CameraFollowing.cs
@@ -1,20 +1,47 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
+[RequireComponent(typeof(Camera))]
 public class CameraFollowing : MonoBehaviour
 {
     [SerializeField]
     private Transform target;
+    private new Transform transform;
     private float speed;
+
+    [Space(5)]
+
+    [SerializeField]
+    private float minScrollValue = 5;
+    [SerializeField]
+    private float maxScrollValue = 10;
+    [SerializeField]
+    private float scale = 1;
+
+    private new Camera camera;
 
     private void Awake()
     {
         speed = target.GetComponent<MovementScript>().Speed;
+        transform = GetComponent<Transform>();
+        camera = GetComponent<Camera>();
     }
 
     private void FixedUpdate()
     {
-        transform.position = Vector3.Lerp(transform.position, new Vector3(target.position.x, target.position.y, -10), speed);
+        base.transform.position = Vector3.Lerp(base.transform.position, new Vector3(target.position.x, target.position.y, transform.position.z), speed);
+    }
+
+    private void Update()
+    {
+        float scrollValue = Input.mouseScrollDelta.y;
+        Debug.Log(scrollValue);
+        if (scrollValue > 0 && camera.orthographicSize > minScrollValue)
+        {
+            camera.orthographicSize -= scale;
+        }
+        else if (scrollValue < 0 && camera.orthographicSize < maxScrollValue)
+        {
+            camera.orthographicSize += scale;
+        }
     }
 }
