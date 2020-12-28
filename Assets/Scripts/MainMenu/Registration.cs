ad175e55aa32531a7fa670b8ede282510e73e524
KDANext <dima.1998.gig@mail.ru>
2020-12-28 15:10:36 +0400

    StartWork

--- Assets/Scripts/MainMenu/Registration.cs
+++ Assets/Scripts/MainMenu/Registration.cs
@@ -1,24 +1,20 @@
-﻿using Database;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using TMPro;
 
 public class Registration : MonoBehaviour
 {
-    [SerializeField]
-    private TMP_Text Login;
-    [SerializeField]
-    private TMP_Text Password;
-    [SerializeField]
-    private TMP_Text NickName;
-    public void ButtonRegister_Click()
+    private readonly string APINameFunction = "Register";
+    private readonly string APINameController = "Client";
+
+    public IEnumerator Register()
     {
-        APIClient.PostRequest("api/Client/Register", new User
+        string url = APIConfig.url();
+        using (WWW www = new WWW(url))
         {
-            Login = Login.text,
-            Password = Password.text,
-            Nickname = NickName.text,
-        });
+            yield return www;
+            Renderer renderer = GetComponent<Renderer>();
+            renderer.material.mainTexture = www.texture;
+        }
     }
 }
