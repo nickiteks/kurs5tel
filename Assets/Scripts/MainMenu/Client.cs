5e8e15c7f9b963f959e0463f34b609822f871ac0
KDANext <dima.1998.gig@mail.ru>
2020-12-29 11:56:32 +0400

    Autorization and registration
    Work is done

--- Assets/Scripts/MainMenu/Client.cs
+++ Assets/Scripts/MainMenu/Client.cs
@@ -5,23 +5,5 @@ using UnityEngine;
 
 public class Client : Singleton<Client>
 {
-    private void Awake()
-    {
-        DontDestroyOnLoad(this.gameObject);
-    }
-
     public User user;
-    public bool isNewGame { get; private set; }
-
-    public void NewGame()
-    {
-        isNewGame = true;
-    }
-
-    public void ContinueGame()
-    {
-        isNewGame = false;
-    }
-
-
 }
