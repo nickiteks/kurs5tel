67d3d242faf4e0830c6c0166e15c3eef5aaf5952
KDANext <dima.1998.gig@mail.ru>
2020-12-29 11:56:32 +0400

    Autorization and registration
    Work is done

---
+++ Assets/Scripts/MainMenu/Autorization.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using Database;
+
+public class Autorization : MonoBehaviour
+{
+    [SerializeField]
+    private TMP_Text Login;
+    [SerializeField]
+    private TMP_Text Password;
+    public void ButtonAutorization_Click()
+    {
+        try
+        {
+            Client.Instance.user = APIClient.GetRequest<User>($"api/Client/Login?login={Login.text}&password={Password.text}");
+        } catch
+        {
+            Debug.Log("Неправильный логин или пароль");
+        }        
+    }
+}
---
+++ Assets/Scripts/MainMenu/Client.cs
@@ -0,0 +1,9 @@
+﻿using Database;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Client : Singleton<Client>
+{
+    public User user;
+}
