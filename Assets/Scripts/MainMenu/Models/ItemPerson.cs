a815a24b5adf93f7e95bacb0fad830ea30ac8193
KDANext <dima.1998.gig@mail.ru>
2020-12-29 05:33:00 +0400

    Start Create Api
    HardCode Url For request need more time

---
+++ Assets/Scripts/MainMenu/Models/ItemPerson.cs
@@ -0,0 +1,10 @@
+﻿namespace Database
+{
+    public class ItemPerson
+    {
+        public int Id { get; set; }
+        public int PersonId { get; set; }
+        public int ItemId { get; set; }
+        public int ItemCount { get; set; }
+    }
+}
---
+++ Assets/Scripts/MainMenu/Models/User.cs
@@ -0,0 +1,11 @@
+﻿namespace Database
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public string Nickname { get; set; }
+        public int LocationId { get; set; }
+    }
+}
