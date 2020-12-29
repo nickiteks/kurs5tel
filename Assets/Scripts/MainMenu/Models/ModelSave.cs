e164b49a754e3285cd143f387628e78b3d65a45f
KDANext <dima.1998.gig@mail.ru>
2020-12-29 05:33:00 +0400

    Start Create Api
    HardCode Url For request need more time

---
+++ Assets/Scripts/MainMenu/Models/ModelSave.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Database
+{
+    class ModelSave
+    {
+        public User User { get; set; }
+        public List<Person> Persons { get; set; }
+        public List<List<ItemPerson>> InventoryPersons { get; set; }
+        public List<ItemUser> InventoryUsers { get; set; }
+    }
+}
