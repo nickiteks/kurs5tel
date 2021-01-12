297d72123ac6601892abccf0a044fa94947c95e7
Cmanchik <aksenova1648@gmail.com>
2021-01-12 22:11:04 +0300

    Реализация миникарты

--- Assets/Scripts/Map/MapController.cs
+++ Assets/Scripts/Map/MapController.cs
@@ -9,29 +9,29 @@ public class MapController : MonoBehaviour
     private PlayerController playerController;
 
     [SerializeField]
-    private RectTransform mapPanel;
-
+    private Canvas mapCanvas;
     [SerializeField]
     private Image mapImage;
 
     private bool isMapOpen = false;
 
-    private void Awake()
+    private void Start()
     {
-        mapImage.sprite = Resources.Load("MapImage/" + SceneManager.GetActiveScene().name + "Map") as Sprite;
+        Texture2D texture = Resources.Load("MapImage/" + SceneManager.GetActiveScene().name) as Texture2D;
+        mapImage.sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), Vector2.zero);
     }
 
     private void Update()
     {
         if (!isMapOpen && !playerController.IsInteractionWithWorld && Input.GetKeyDown(InputManager.Instance.map))
         {
-            UIManager.Instance.OpenPanel(mapPanel);
+            UIManager.Instance.OpenCanvas(mapCanvas);
             playerController.IsInteractionWithWorld = true;
             isMapOpen = true;
         }
         else if (isMapOpen && Input.GetKeyDown(InputManager.Instance.map))
         {
-            UIManager.Instance.ClosePanel(mapPanel);
+            UIManager.Instance.CloseCanvas(mapCanvas);
             playerController.IsInteractionWithWorld = false;
             isMapOpen = false;
         }
